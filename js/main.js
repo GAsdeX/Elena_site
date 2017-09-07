6150550d2a9b6526b3ab23b79c2d47df62615a9a
Hryhorii Chaikovskyi <hryhorii.chaikovskyi@s4b.team>
2017-09-07 09:38:15 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log($('.site-content .portfolio-section .slide-view'));
+$('.site-content .portfolio-section .slide-view').slideUp();
+$('.site-content .portfolio-section .slide-view').slick({
+
+});
+$('.site-content .portfolio-section .slider-nav').slick({
+
+});
